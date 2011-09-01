f383f1711a36118e19234dea6faa91363c9de830
Hiroki Horiuchi <x19290@gmail.com>
2011-09-01 12:29:26 +0900

    (*eval '((lambda (x) x) 1) ()) (*eval '((lambda (x y) (cons x y)) 1 2) dummy-env) consのようなものは、移譲すればいいが、 defineのようなものは(おそらくdefineだけでいいと思うが) 別途定義しなければならない。 まだ定義してないので本格的な実験はできない。

--- eval.scm
+++ eval.scm
@@ -1,14 +1,28 @@
 (define undef (if #f #f))
 
-(define dummy-pad (make-hash-table))
-(hash-table-put! dummy-pad 'x 123)
-(define dummy-env (cons dummy-pad ()))
+(define globals (make-hash-table))
+
+(define dummy-env (cons globals ()))
+
+(define dummy-env
+  (let ((pad #f))
+    (set! pad (make-hash-table))
+    (hash-table-put! pad 'x 123)
+    (cons pad dummy-env)))
+
+(define built-in '(cons))
+
+(dolist
+ (symbol built-in)
+ (hash-table-put!
+  globals symbol
+  (cons 'delegate (eval symbol (interaction-environment)))))
 
 (define (*eval form env)
   (cond
    ((or (null? form) (boolean? form) (number? form)) form)
    ((symbol? form) (binding form env))
-   ((pair? form) (eval-pair (car form) (cdr form) env))
+   ((pair? form) (eval-pair (car form) form env))
    (else
     (error "unknown atom"))))
 
@@ -17,21 +31,31 @@
    ((null? env) undef)
    (else
     (let ((pad (car env)))
-      (let ((got (hash-table-get pad symbol)))
+      (let ((got (hash-table-get pad symbol undef)))
 	(cond
 	 ((eq? undef got)
 	  (binding symbol (cdr env)))
 	 (else
 	  got)))))))
 
-(define (eval-pair xcar xcdr env)
+(define (eval-pair xcar form env)
   (cond
-   ((eq? 'quote xcar) (car xcdr))
-   ((eq? 'cond xcar) (eval-cond xcdr env))
-   ((eq? 'lambda xcar) (make-closure xcdr env))
-   (*apply
-    (if (symbol? xcar) (binding xcar) xcar)
-    (args xcdr env))))
+   ((eq? 'lambda xcar) (make-closure form env))
+   (else
+    (let ((xcdr (cdr form)))
+      (cond
+       ((eq? 'quote xcar) (car xcdr))
+       ((eq? 'cond xcar) (eval-cond xcdr env))
+       (else
+	(*apply
+	 (*eval xcar env)
+	 (args xcdr env))))))))
+
+(define (args xargs env)
+  (cond
+   ((null? xargs) ())
+   (else
+    (cons (*eval (car xargs) env) (args (cdr xargs) env)))))
 
 (define (progn forms env)
   (cond
@@ -62,10 +86,32 @@
       (else (eval-cond-body clauses env)))))
  
 (define (*apply function args)
-  (print "stub apply"))
+  (let ((xcar (car function)))
+    (cond
+     ((eq? xcar 'delegate)
+      (apply (cdr function) args))
+     (else
+      (let*
+	  ((xcdr (cdr function))
+	   (lambda (car xcdr))
+	   (env
+	    (cons
+	     (make-pad (cadr lambda) args)
+	     (cdr xcdr)))) ; old env
+	(progn (cddr lambda) env))))))
 
-(define (make-closure spec env)
-  (cons spec env))
+(define (make-pad params values)
+  (define
+    (make-pad-iter pad params values)
+    (cond
+     ((null? params) pad)
+     (else
+      (hash-table-put! pad (car params) (car values))
+      (make-pad-iter pad (cdr params) (cdr values)))))
+  (make-pad-iter (make-hash-table) params values))
+
+(define (make-closure lambda-form env)
+  (cons 'closure (cons lambda-form env)))
 
 (*eval () ())
 (*eval #t ())
@@ -77,24 +123,43 @@
 (*eval
  '(cond
    (else #f #t))
- ())
+ ()
+)
 (*eval
  '(cond
    (#f ...)
    (else #f #t))
- ())
+ ()
+)
 (*eval
  '(cond
    (#f ...)
    (#t #f #t)
    (#t ...))
- ())
-(*eval
+ ()
+)
+(*eval ; error expected
  '(cond
    (else)
    (#t 123))
  ()
 )
+(*eval '(lambda ()) dummy-env)
+(*eval
+ '(
+   (lambda (x) #f #t)
+   123)
+ dummy-env
+)
+(*eval '(cons 1 2) dummy-env)
+(*eval '
+ (
+  (lambda (x) x)
+  123)
+ dummy-env
+)
+(*eval '((lambda (x) x) 1) ())
+(*eval '((lambda (x y) (cons x y) 1 2) dummy-env)
 (
 	(
 		(lambda (x) (lambda () x))
