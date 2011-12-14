1dba4246650fec4965b9d05525e2b33083bd9535
Hiroki Horiuchi <x19290@gmail.com>
2011-12-14 22:35:11 +0900

    組み込みの特殊形式と組み込みの関数の扱いを整理した。 チェックが甘い所はあるが、行数は少し減ったはず。 組み込みの特殊形式は quote と if だけ。たぶん他はマクロで書ける。 *eval-pair -> *try-special -> hash-table-get *keywords で失敗すると、 引数を全部 *eval して (map) *apply に渡す。 現在マクロは使えないが、どうしたら良いかだいたい分かっている。

--- 1error.scm
+++ 1error.scm
@@ -1,2 +1,4 @@
 #?=(*eval '(if #t (car '(car . cdr)) (car ())) *base-env)
-#?=(*eval '(if #f (car '(car . cdr)) (car ())) *base-env)
+#?=(guard
+    (_ (else 'caught))
+    (*eval '(if #f (car '(car . cdr)) (car ())) *base-env))
--- eval.scm
+++ eval.scm
@@ -1,11 +1,42 @@
 (use util.match)
 
-(define (*eval form env)
+(define *keywords (make-hash-table))
+
+(hash-table-put!
+ *keywords 'quote '(
+ (_ form)
+ form))
+ 
+(hash-table-put!
+ *keywords 'if '(
+ (env pred on-true on-false)
+ (if (*eval pred env) (*eval on-true env) (*eval on-false env))))
+
+(define (*eval-pair car cdr env)
   (cond
-   ((*literal? form) form)
-   ((symbol? form) (*binding form env))
-   ((pair? form) (*eval-pair form env))
-   (else (*error form "syntax: bad form"))))
+   ((eq? car 'lambda) (cons 'closure (cons env cdr)))
+  (else
+   (guard
+    (_
+     (else
+      (*apply
+       (*eval car env)
+       (map (lambda (arg) (*eval arg env)) cdr))))
+    (*try-special car cdr env)))))
+
+(define (*try-special car cdr env)
+  (define (trigger) (list 'quote (cons env cdr)))
+  (define (clauses) (hash-table-get *keywords car))
+  (eval
+   (list 'match (trigger) (clauses))
+   (interaction-environment)))
+
+(define (*eval form env)
+  (match
+   form
+   ((car . cdr) (*eval-pair car cdr env))
+   ((? *literal? _) form)
+   (_ (*binding form env))))
 
 (define (*literal? form)
   (or
@@ -14,52 +45,19 @@
    (number? form)))
 
 (define (*binding var env)
+  (call/cc
+   (lambda (x)
+     (for-each
+      (lambda (frame)
+	(if (not (hash-table? frame)) (*error frame "broken data: frame"))
+	(guard (_ (else)) (x (hash-table-get frame var))))
+      env)
+     (*error var "unbound"))))
+
+(define (*apply func args)
   (cond
-   ((null? env) (*error env "no more frame"))
-   ((not (pair? env)) (*error env "broken data: env"))
-   (else
-    (let ((frame (car env)))
-      (if (not (hash-table? frame)) (error frame "broken data: frame"))
-      (guard
-       (_
-	(else
-	 (*binding var (cdr env))))
-       (hash-table-get frame var))))))
-
-(define (*eval-pair form env)
-  (match
-   form
-   (('lambda params body . _)
-    (list 'closure env params body))
-   (_
-    (*apply form env))))
-
-(define (*null-or-pair? s)
-  (or (null? s) (pair? s)))
-
-(define (*apply form env)
-  (match
-   form
-   ((func . args)
-    (match
-     (*eval func env)
-     ((func-tag . func-body)
-      (let ((args2 (*eval-args-or-not func-tag args env)))
-	(match
-	 func-tag
-	 ('closure (*apply-closure func-body args2))
-	 ('special (apply func-body (list args env)))
-	 ((or 'misc 'primitive)
-	  (apply func-body args2))
-	(_ (*error func-tag "syntax: broken func-tag")))))
-     (_ (*error func "syntax: broken func"))))
-   (_ (*error form "syntax: broken application"))))
-
-(define (*eval-args-or-not func-tag args env)
-  (match
-   func-tag
-   ((or 'special 'misc) args)
-   (_  (*eval-args args env))))
+   ((or (subr? func) (closure? func)) (apply func args))
+   (else (*apply-closure (cdr func) args))))
 
 (define (*apply-closure func-body args)
   (call-with-values
@@ -72,8 +70,8 @@
     (lambda (env params form)
       (*eval form (cons (*frame (make-hash-table) params args) env)))))
 
-(define (*eval-args args env)
-  (map (lambda (var) (*eval var env)) args))
+(define (*null-or-pair? s)
+  (or (null? s) (pair? s)))
 
 (define (*frame frame params args)
   (cond
@@ -94,49 +92,22 @@
    (else
     (*error params "syntax: broken params"))))
 
-(define (*error s m)
-  (error "*MINIEVAL*" s m))
+(define (*error a b)
+  (error "*MINIEVAL*" a b))
 
-(define *base-funcs
-  (let
-      ((e (interaction-environment))
-       (frame (make-hash-table)))
-    (for-each
-     (lambda (name)
-       (hash-table-put! frame name (cons 'primitive (eval name e))))
-     '(car cdr cons eq? pair? = + - print))
-    frame))
+(define *base-frame (make-hash-table))
 
-(define *base-misc
-  (let ((frame (make-hash-table)))
-    (hash-table-put! frame 'quote (cons 'misc identity))
-    frame))
+(hash-table-put! *base-frame 'car car)
+(hash-table-put! *base-frame 'cdr cdr)
+(hash-table-put! *base-frame 'cons cons)
+(hash-table-put! *base-frame 'eq? eq?)
+(hash-table-put! *base-frame '= =)
+(hash-table-put! *base-frame '+ +)
+(hash-table-put! *base-frame '- -)
+(hash-table-put! *base-frame 'print print)
 
-(define *base-specials
-  (let ((frame (make-hash-table)))
-    (hash-table-put!
-     frame
-     'if
-     (cons
-      'special
-      (lambda (args env)
-	(match
-	 args
-	 ((pred t f) (if (*eval pred env) (*eval t env) (*eval f env)))
-	 (_ (*error args "syntax: if"))))))
-    (hash-table-put!
-     frame
-     'define
-     (cons
-      'special
-      (lambda (args env)
-	(match
-	 args
-	 ((name expression)
-	  (hash-table-put! *base-user name (*eval expression env)))
-	 (_ (*error args "syntax: define"))))))
-    frame))
+(hash-table-put!
+ *base-frame '*define
+ (lambda (var val) (hash-table-put! *base-frame var val)))
 
-(define *base-user (make-hash-table))
-
-(define *base-env (list *base-user *base-funcs *base-misc *base-specials))
+(define *base-env (list *base-frame))
